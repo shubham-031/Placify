53e7628d201d5f8138950fe6dbeb85f7d256a964
shubham-031 <jadhavshubham9718@gmail.com>
2026-07-06 00:22:28 +0530

    ok6

--- server/server.js
+++ server/server.js
@@ -49,12 +49,16 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://placify-beta.vercel.app",
+      "https://placify-git-shubham1-shubhams-projects-a1a81d1d.vercel.app",
+      "https://placify-9hpttmrxr-shubhams-projects-a1a81d1d.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+
+app.options("*", cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
