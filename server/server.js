8819846a50a18f9a4fe871a6b2ad32baf52e6722
shubham-031 <jadhavshubham9718@gmail.com>
2026-07-06 13:37:33 +0530

    ok9

--- server/server.js
+++ server/server.js
@@ -59,6 +59,10 @@ app.use(
 );
 
 app.options("*", cors());
+app.use((req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+});
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
